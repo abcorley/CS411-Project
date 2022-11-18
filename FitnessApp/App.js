d555fa09791f9e27b64c45d7b8def2d2302783c5
Araceli Corley <araceli@crc-dot1x-nat-10-239-199-151.bu.edu>
2022-11-18 13:09:20 -0500

    Added main code for project

---
+++ FitnessApp/App.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { NavigationContainer, View, Text } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './components/HomeScreen';
+import WorkoutResultsScreen from './components/WorkoutResultsScreen';
+import CreateWorkoutScreen from './components/CreateWorkoutScreen';
+
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="CreateWorkout" component={CreateWorkoutScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+     <Stack.Navigator>
+        <Stack.Screen
+          name="Root"
+          component={Tabs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="WorkoutResults" component={WorkoutResultsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
