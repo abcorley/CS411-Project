5c5bed059242611a21678f0abbb5c7fba653364d
Araceli Corley <araceli@crc-dot1x-nat-10-239-239-100.bu.edu>
2022-12-08 16:05:35 -0500

    Added LoginStyleSheet

---
+++ FitnessApp/stylesheets/LoginStyleSheet.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+const LoginStyleSheet = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alginItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alginItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontsize: 16,
+  },
+  buttonOutlineText: {},
+});
+
+export default LoginStyleSheet;
